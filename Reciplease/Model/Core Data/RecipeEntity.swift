4c512aff744e6d03db7c204e2227552a8e12da37
Ousama boujaouane <oboujaouane@gmail.com>
2020-10-25 17:07:49 +0100

    Add RecipeEntity file with save function

---
+++ Reciplease/Model/Core Data/RecipeEntity.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeEntity.swift
+//  Reciplease
+//
+//  Created by ousama boujaouane on 25/10/2020.
+//  Copyright © 2020 Sajid. All rights reserved.
+//
+
+import CoreData
+
+class RecipeEntity: NSManagedObject {
+
+    static func save(_ recipe: Recipe) {
+        let recipeEntity = RecipeEntity(context: AppDelegate.viewContext)
+        recipeEntity.image_url = recipe.image
+        for ingredient in recipe.ingredientLines {
+            recipeEntity.ingredient_lines! += "\(ingredient) " // TODO: remove exclamation
+        }
+        recipeEntity.name = recipe.label
+        recipeEntity.original_url = recipe.url
+        recipeEntity.preparation_time = Int16(recipe.totalTime)
+        recipeEntity.yield = Int16(recipe.yield)
+        saveContext()
+    }
+
+    static func saveContext() {
+            try? AppDelegate.viewContext.save()
+    }
+}
