9aae9c3061fb3b1b36b441ef3338ae152b8928b5
Ousama boujaouane <oboujaouane@gmail.com>
2020-11-02 15:36:02 +0100

    Fix bug on get recipes request

--- Reciplease/Model/WebService/EdamamService.swift
+++ Reciplease/Model/WebService/EdamamService.swift
@@ -19,41 +19,17 @@ class EdamamService {
                                     "app_id": APIKey.EdamamRecipeSearchAppID,
                                     "q": ingredients.joined(separator: ",")]
         
-        //        AF.request(Edamam.endpoint, method: .get, parameters: ingredientsParameter).validate().responseJSON { response in
-        //            guard let data = response.data, response.error == nil else {
-        //                callback(false, nil)
-        //                return
-        //            }
-        //
-        //            guard let response = response.response, response.statusCode == 200 else {
-        //                callback(false, nil)
-        //                return
-        //            }
-        //
-        //            guard let recipes = try? JSONDecoder().decode(Recipes.self, from: data) else {
-        //                callback(false, nil)
-        //                return
-        //            }
-        //
-        //            callback(true, recipes)
-        //        }
-        
         AF.request(Edamam.endpoint, method: .get, parameters: ingredientsParameter)
             .validate(contentType: ["application/json"])
             .responseJSON { response in
-                switch response.result {
-                case .success:
-                    guard let data = response.data,
-                        let recipes = try? JSONDecoder().decode(Recipes.self, from: data) else {
-                            callback(false, nil)
-                            return
-                    }
+                guard let data = response.data else {
+                    return
+                }
+                do {
+                    let decoder = JSONDecoder()
+                    let recipes = try decoder.decode(Recipes.self, from: data)
                     callback(true, recipes)
-                case .failure(let error):
-                    let errorCode = error._code
-                    let errorDomain = error._domain
-                    let userInfo = error._userInfo
-                    print("Request failed with error: \(error), code: \(errorCode), domain: \(errorDomain), userInfo: \(String(describing: userInfo))")
+                } catch {
                     callback(false, nil)
                 }
         }
